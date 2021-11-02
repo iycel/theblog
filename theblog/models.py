eabf1b6e652abaf343ea927fd862fef202e1d25b
iycel <ismailycel@gmail.com>
2021-11-03 01:44:35 +0300

    feat : Category

--- theblog/models.py
+++ theblog/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
-
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name='Title')
     title_tag = models.CharField(max_length=50, default='Awkward Blog from Awkward Men', verbose_name='Title Tag')
@@ -11,28 +12,25 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_pic', blank=True, verbose_name='Image')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Login User')
     post_date = models.DateField(auto_now_add=True)
-    CATEGORY = {
-        ('c1', 'Cat c1'),
-        ('c2', 'Cat c2')
-    }
-
-    category = models.CharField(choices=CATEGORY,max_length=50, verbose_name='Category')
+    
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Category')
     
     STATUS = {
-        ('s1', 'Sta s1'),
-        ('s2', 'Sta s2')
+        ('d', 'Draft'),
+        ('p', 'Published')
     }
 
-    status= models.CharField(max_length=50, choices=STATUS, verbose_name='Status')
+    status= models.CharField(max_length=50, choices=STATUS, verbose_name='Status', default="d")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
-    def get_absolute_url(self):
-        return reverse("post_details", args=(str(self.id)))
+    # def get_absolute_url(self):
+    #     return reverse("post_details", args=(str(self.id)))
     
-    class META :
+    class Meta :
         ordering = ['-post_date']
+
     
 
 
